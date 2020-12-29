40b24f1baedf3aa987658375be7b448ff53ecf36
Ado Pabianko <adopabianko@gmail.com>
2020-12-29 21:29:49 +0700

    login with sosial media

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'token',
+        'register_from'
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Http\Request;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/auth/redirect', function (Request $request) {
+    return Socialite::driver($request->sosmed)->redirect();
+})->name('auth.redirect');
+
+Route::get('/auth/callback/{sosmed}', function (Request $request) {
+    $sosmed = $request->sosmed;
+    $user = Socialite::driver($sosmed)->user();
+
+    $user = User::firstOrCreate([
+        'email' => $user->email
+    ],[
+        'name' => $user->name,
+        'password' => Hash::make('secret'),
+        'token' => Hash::make('email'),
+        'register_from' => $sosmed,
+    ]);
+
+    Auth::login($user, true);
+
+    return redirect('home');
 });
